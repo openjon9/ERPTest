7b761da5bab6ac50722678a23bf0190fd1925abc
蛋蛋 <egg@wang-haishinteki-macbook-pro.local>
2018-05-24 12:18:08 +0800

    Initial Commit

---
+++ ERP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ERP
+//
+//  Created by 蛋蛋 on 2018/5/24.
+//  Copyright © 2018年 蛋蛋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
