8643e1fb43234a43bc848ab07c533fb3eb8ad6c5
蛋蛋 <egg@wang-haishinteki-macbook-pro.local>
2018-07-30 10:59:03 +0800

    2018/07/30

--- ERP/ViewController.swift
+++ ERP/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,WKNavigationDelegate{
+    let mURL = "http://erp.coder.com.tw/"
+    @IBOutlet weak var myWebView: WKWebView!
+    
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myWebView.navigationDelegate = self
+       if let murl = URL(string: mURL){
+          let request = URLRequest(url: murl)
+            myWebView.load(request)
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        myActivityIndicator.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        myActivityIndicator.stopAnimating()
+    }
+    
+    override var prefersStatusBarHidden: Bool{
+        return true
     }
 
     override func didReceiveMemoryWarning() {
